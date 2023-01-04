9ad5360afc13ee3dd0ff6f45be26c511b20c8a67
João Victor <jvmarcolino@outlook.com>
2023-01-03 23:48:30 -0300

    first exercise with typescript

---
+++ exercicio01.js
@@ -0,0 +1,9 @@
+// function soma (number1, number2 :number) {
+//     console.log(number1 + number2)
+// }
+// soma(5,4)
+// Correção do exercicio
+function soma(number1, number2) {
+    return number1 + number2;
+}
+console.log(soma(3, 5));
---
+++ exercicio01.ts
@@ -0,0 +1,14 @@
+// function soma (number1, number2 :number) {
+//     console.log(number1 + number2)
+// }
+
+// soma(5,4)
+
+
+// Correção do exercicio
+
+function soma (number1 : number, number2 : number){
+    return number1 + number2
+}
+
+console.log(soma(3,5))
