236851efee3ab4d080637dfb93006bed97145436
João Victor <jvmarcolino@outlook.com>
2023-01-04 23:41:35 -0300

    Fundamentos TS

---
+++ 2_fundamentos/index.js
@@ -0,0 +1,21 @@
+//  1- numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.654654;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
+// 2-String
+var firstName = "João";
+console.log(firstName.toUpperCase());
+var lastName = "Victor";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+// 3-Boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
---
+++ 2_fundamentos/index.ts
@@ -0,0 +1,45 @@
+//  1- numbers
+
+let x: number = 10
+
+console.log(x)
+
+x = 16
+
+console.log(typeof x)
+
+const y:number = 15.654654
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toPrecision(3))
+
+// 2-String
+
+const firstName: string = "João"
+
+console.log(firstName.toUpperCase())
+
+const lastName: string = "Victor"
+
+let fullName: string = firstName + " " + lastName
+
+console.log(fullName)
+
+// 3-Boolean
+
+let a: boolean = false
+
+console.log(a)
+console.log(typeof a)
+
+a = true
+console.log(a)
+
+// 4-inference e annotation
+
+let ann: string = "teste"
+
+let inf = "testeee"
