7e0f0ed7a5e4b557582b7e618f290d28143b1a03
João Victor <jvmarcolino@outlook.com>
2023-02-07 22:39:04 -0300

    Continuação das classes. (Visibilidade)

--- 8_classes/index.js
+++ 8_classes/index.js
@@ -59,3 +59,143 @@ const jimmy = new Dwarf("Jimmy");
 console.log(jimmy);
 jimmy.greeting();
 console.log(jimmy);
+// 6-This
+class Truck {
+    constructor(model, hp) {
+        this.model = model;
+        this.hp = hp;
+    }
+    showDetails() {
+        console.log(`Caminhão modelo ${this.model}, tem ${this.hp} cavalos de potência.`);
+    }
+}
+const volvo = new Truck("Volvo", 450);
+const scania = new Truck("Scania", 480);
+volvo.showDetails();
+scania.showDetails();
+// 7-Getters
+class Person {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+    get nomeCompleto() {
+        return this.nome + " " + this.sobrenome;
+    }
+}
+const joaoVictor = new Person("João", "Victor");
+console.log(joaoVictor.nome);
+console.log(joaoVictor.nomeCompleto);
+// 8-Setter
+class Coords {
+    set fillX(x) {
+        if (x === 0) {
+            return;
+        }
+        this.x = x;
+        console.log("Coordenada X adicionada com sucesso");
+    }
+    set fillY(y) {
+        if (y === 0) {
+            return;
+        }
+        this.y = y;
+        console.log("Coordenada Y adicionada com sucesso");
+    }
+    get getCoords() {
+        return `X: ${this.x} e Y: ${this.y}`;
+    }
+}
+const myCoords = new Coords();
+myCoords.fillX = 15;
+myCoords.fillY = 10;
+console.log(myCoords);
+console.log(myCoords.getCoords);
+class blogPost {
+    constructor(title) {
+        this.title = title;
+    }
+    itemTitle() {
+        return `O titulo do post é ${this.title}`;
+    }
+}
+const myPost = new blogPost("Hello World");
+console.log(myPost.itemTitle());
+class TestingInterface {
+    constructor(title) {
+        this.title = title;
+    }
+    itemTitle() {
+        return `O titulo do post é ${this.title}`;
+    }
+}
+const anotherPost = new TestingInterface("Um novo começo");
+console.log(anotherPost.itemTitle());
+// 10- Override de métodos 
+class Base {
+    someMethod() {
+        console.log("Alguma coisa");
+    }
+}
+class Nova extends Base {
+    someMethod() {
+        console.log("Outra coisa");
+    }
+}
+const myObject = new Nova();
+myObject.someMethod();
+// 11- Visibilidade: Public
+class C {
+    constructor() {
+        this.x = 10;
+    }
+}
+class D extends C {
+}
+const instC = new C();
+console.log(instC);
+const instD = new D();
+console.log(instD);
+// 12- Visibilidade: Protected
+class E {
+    constructor() {
+        this.x = 10;
+    }
+    protectedMethod() {
+        console.log(`Esse método é protegido.`);
+    }
+}
+class F extends E {
+    showX() {
+        console.log(`O x é ${this.x}`);
+    }
+    showMethod() {
+        this.protectedMethod();
+    }
+}
+const instF = new F();
+instF.showMethod();
+// 13- Visibilidade: Private
+class PrivateClass {
+    constructor() {
+        this.name = "Private";
+    }
+    showName() {
+        return this.name;
+    }
+    privateMethod() {
+        return `Metodo privado`;
+    }
+    showPrivateMethod() {
+        return this.privateMethod();
+    }
+}
+const privateObj = new PrivateClass();
+// console.log(privateObj.name)
+console.log(privateObj.showName());
+console.log(privateObj.showPrivateMethod());
+// class Testing extends PrivateClass {
+//     myMethod() {
+//         this.privateMethod()
+//     }
+// }
--- 8_classes/index.ts
+++ 8_classes/index.ts
@@ -101,3 +101,222 @@ console.log(jimmy)
 jimmy.greeting()
 
 console.log(jimmy)
+
+
+// 6-This
+
+class Truck {
+    model 
+    hp
+
+    constructor(model: string, hp: number) {
+        this.model = model
+        this.hp = hp
+    }
+
+    showDetails() {
+        console.log(`Caminhão modelo ${this.model}, tem ${this.hp} cavalos de potência.`)
+    }
+}
+
+const volvo = new Truck("Volvo", 450)
+const scania = new Truck("Scania", 480)
+
+volvo.showDetails()
+scania.showDetails()
+
+
+// 7-Getters
+
+class Person {
+    nome
+    sobrenome 
+
+    constructor(nome:string, sobrenome: string) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+
+    get nomeCompleto() {
+        return this.nome + " " + this.sobrenome
+    }
+}
+
+const joaoVictor = new Person("João", "Victor")
+
+console.log(joaoVictor.nome)
+
+console.log(joaoVictor.nomeCompleto)
+
+
+// 8-Setter
+
+class Coords {
+    x!: number
+    y!: number
+
+    set fillX(x: number) {
+        if(x === 0) {
+            return
+        }
+
+        this.x = x
+
+        console.log("Coordenada X adicionada com sucesso")
+    }
+
+    set fillY(y: number) {
+        if(y === 0) {
+            return
+        }
+
+        this.y = y
+
+        console.log("Coordenada Y adicionada com sucesso")
+    }
+
+    get getCoords() {
+        return `X: ${this.x} e Y: ${this.y}`
+    }
+}
+
+const myCoords = new Coords()
+
+myCoords.fillX = 15
+myCoords.fillY = 10
+
+console.log(myCoords)
+console.log(myCoords.getCoords)
+
+// 9-Herdando Interfaces
+
+interface showTitle {
+    itemTitle(): string
+}
+
+class blogPost implements showTitle {
+    title
+
+    constructor(title: string) {
+        this.title = title
+    }
+
+    itemTitle() {
+        return `O titulo do post é ${this.title}`
+    }
+}
+
+const myPost = new blogPost("Hello World")
+
+console.log(myPost.itemTitle())
+
+class TestingInterface implements showTitle {
+    title
+
+    constructor(title: string) {
+        this.title = title
+    }
+
+    itemTitle() {
+        return `O titulo do post é ${this.title}`
+    }
+}
+
+const anotherPost = new TestingInterface("Um novo começo")
+
+console.log(anotherPost.itemTitle())
+
+
+// 10- Override de métodos 
+class Base {
+    someMethod() {
+        console.log("Alguma coisa")
+    }
+}
+
+class Nova extends Base {
+    someMethod() {
+        console.log("Outra coisa")
+    }
+}
+
+const myObject = new Nova()
+
+myObject.someMethod()
+
+
+// 11- Visibilidade: Public
+
+class C {
+    x = 10
+}
+
+class D extends C {
+}
+
+const instC = new C()
+
+console.log(instC)
+
+const instD = new D()
+
+console.log(instD)
+
+ 
+// 12- Visibilidade: Protected
+
+class E {
+    protected x = 10
+
+    protected protectedMethod() {
+        console.log(`Esse método é protegido.`)
+    } 
+}
+
+class F extends E {
+
+    showX() {
+        console.log(`O x é ${this.x}`)
+    }
+
+    showMethod() {
+        this.protectedMethod()
+    }
+}
+
+const instF = new F()
+
+instF.showMethod()
+
+
+// 13- Visibilidade: Private
+
+class PrivateClass {
+    private name = "Private"
+
+    showName() {
+        return this.name
+    }
+
+    private privateMethod() {
+        return `Metodo privado`
+    }
+
+    showPrivateMethod() {
+        return this.privateMethod()	
+    }
+
+}
+
+const privateObj = new PrivateClass()
+
+// console.log(privateObj.name)
+console.log(privateObj.showName())
+console.log(privateObj.showPrivateMethod())
+
+// class Testing extends PrivateClass {
+//     myMethod() {
+//         this.privateMethod()
+//     }
+// }
+
